776634277db023c65aeb9d456839630f653abec0
theerawat.a@cdg.co.th <theerawat.a@cdg.co.th>
2024-02-23 13:19:27 +0700

    update path

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     component: () => import("@/layouts/default/Default.vue"),
-    redirect: "/booking",
+    redirect: "/booking/officer",
     children: [...booking, ...officer],
   },
 ];
